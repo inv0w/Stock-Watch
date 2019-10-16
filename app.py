9027f583e5505e786af6a4096f74ab09c9cb61c7
inv0w <jonjoninfante@gmail.com>
2019-10-15 22:56:15 -0700

    Stock returns value, added exception for non-stock inputs

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template, request
 import requests
 import json
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
+import matplotlib.pyplot as plt
 
+
+AV_key = '3CSH2Q79FWBCUE4M'
+ts = TimeSeries(key=AV_key, output_format='pandas')
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
+
+@app.route('/stock')
+def index():
+    search_term = request.args.get("search")
+    try:
+        data, meta_data = ts.get_intraday(symbol=search_term, interval='5min')
+    except:
+        return render_template("home.html")
+    current_price = data['4. close'].iloc[0] #Current stock price
+    print(data)
+    print(meta_data)
+    return render_template("index.html", current_price=current_price, stock=search_term)
+
+
+# data['4. close'].plot()
+# plt.title('Intraday Times Series for the MSFT stock (1 min)')
+# plt.show()
