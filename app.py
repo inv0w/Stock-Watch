582f0bfc0322d1be12d5d893909466e5c7149dc9
inv0w <jonjoninfante@gmail.com>
2019-10-17 19:21:43 -0700

    Added Error Page

--- app.py
+++ app.py
@@ -24,16 +24,20 @@ def index():
     try:
         data, meta_data = ts.get_intraday(symbol=search_term, interval='60min')
     except:
-        return render_template("home.html")
+        return render_template("error_page.html")
     current_price = data['4. close'].iloc[0] #Current stock price
     stock_price = data['4. close'].values.tolist()
     stock_time = data.index.values
-    print(data)
-    print(meta_data)
-    print(stock_price)
-    print(stock_time)
     return render_template("index.html", current_price=current_price, stock=search_term,
     stock_time=stock_time, stock_price=stock_price)
+
+@app.route('/error_page')
+def error():
+    """Shows the Error Page"""
+    return render_template("error_page.html")
+
+
+
 
 
 # data['4. close'].plot()
